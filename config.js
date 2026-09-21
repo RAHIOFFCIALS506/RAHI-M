dd540a8fcd5a46b167a30b4d67f83dd738427e3f
Rahi officials <rahiofficials506@gmail.com>
2026-09-21 20:01:29 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // ============ TELEGRAM ============
+    TELEGRAM_BOT_TOKEN: "YOUR_TELEGRAM_BOT_TOKEN_HERE",
+    ADMIN_ID: "YOUR_TELEGRAM_ID_HERE",
+
+    // ============ BOT ============
+    BOT_NAME: "𝐑ᴀʜɪ 𝐁ᴏᴛ",
+    PREFIX: ".",
+
+    // ============ OWNER ============
+    OWNER_NUMBER: "8801733727775",
+
+    // ============ SESSION ============
+    SESSION_DIR: "./session"
+};
